73fd7337e39df638c56cecc3669f0282a8be3308
SadiaBe <sadia.islam.badhon@gmail.com>
2020-10-08 11:35:42 -0400

    student1

--- ProblemSolvingPatterns.js
+++ ProblemSolvingPatterns.js
@@ -1,6 +1,9 @@
 
 function validAnagram(str1,str2){
-    // add whatever parameters you deem necessary - good luck!
+    // i forgot to check if the strings were the same length, need to do this before comparing anything else
+    if (str1.length !== str2.length) {
+        return false
+    }
     let count1 = {}
     let count2 = {}
     for (let val of str1) {
@@ -22,3 +25,49 @@ function validAnagram(str1,str2){
     
   }
   validAnagram("love", "evol")
+
+
+
+  // his solution using tirnary? : 
+
+  // function validAnagram(first, second) {
+//   if (first.length !== second.length) {
+//     return false;
+//   }
+//   const lookup = {};
+//   for (let i = 0; i < first.length; i++) {
+//     let letter = first[i];
+//     // if letter exists, increment, otherwise set to 1
+//     lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+//   }
+//   console.log(lookup)
+//   for (let i = 0; i < second.length; i++) {
+//     let letter = second[i];
+//     // can't find letter or letter is zero then it's not an anagram
+//     if (!lookup[letter]) {
+//       return false;
+//     } else {
+//       lookup[letter] -= 1;
+//     }
+//   }
+//   return true;
+// }
+// // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+// validAnagram('anagrams', 'nagaramm')
+
+
+
+//student 1: code is cleaner and still o(n) comlexity 
+// function same(arr1, arr2){
+//     if(arr1.length !== arr2.length){
+//     return false;
+//     }
+//     for(let i = 0; i < arr1.length; i++){
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2)
+//     if(correctIndex === -1) {
+//     return false;
+//     }
+//     arr2.splice(correctIndex,1)
+//     }
+//     return true
+//     }
