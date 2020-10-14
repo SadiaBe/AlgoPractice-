e7e0d04e5edb9a74228c1a80c686d9fc95cf233f
SadiaBe <sadia.islam.badhon@gmail.com>
2020-10-13 22:24:55 -0400

    fixed

--- maxSum.js
+++ maxSum.js
@@ -1,3 +1,25 @@
+
+//this is my atempt 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this is the long way with large big 0 
+
 function maxSubarraySum(arr, num) {
     if ( num > arr.length){
       return null;
@@ -18,4 +40,21 @@ function maxSubarraySum(arr, num) {
   maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
   
   
+//this is the better solution: 
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+      maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+      tempSum = tempSum - arr[i - num] + arr[i];
+      maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+  }
+  
+  maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
   
