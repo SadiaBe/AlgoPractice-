89db78ad56560cafb70993611336d65013b8338c
SadiaBe <sadia.islam.badhon@gmail.com>
2020-10-19 20:11:58 -0400

    added my own explanations

--- countingdown.js
+++ countingdown.js
@@ -1,9 +1,11 @@
 // Recursive Version
 function countDown(num){
+    //check to see if num is less than or equal to 0, if its not then it just goes to the next block of code *this is the base case*
     if(num <= 0) {
         console.log("All done!");
         return;
     }
+    //next block of code is it first prints the num and then subtracks 1 from the num and then recalls itself with the new value of num *the new value of num is the different input 
     console.log(num);
     num--;
     countDown(num);
